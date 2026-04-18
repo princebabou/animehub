a5c438fcc504629280a25c1db83216de5b88041f
princebabou <baboumanzi69@gmail.com>
2026-04-18 14:05:09 +0200

    [fix errors]

--- next.config.js
+++ next.config.js
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["cdn.myanimelist.net"],
+    domains: ["cdn.myanimelist.net", "myanimelist.net"],
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "cdn.myanimelist.net",
+        port: "",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https",
+        hostname: "myanimelist.net",
         port: "",
         pathname: "/images/**",
       },
